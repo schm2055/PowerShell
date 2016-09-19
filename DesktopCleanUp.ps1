01ec775598ed8fc829c4b581d15e84fb80f9a2a2
Andrew Schmitt <dschm2055@gmail.com>
2016-09-19 07:33:14 -0500

    insturctions

--- DesktopCleanUp.ps1
+++ DesktopCleanUp.ps1
@@ -1,2 +1,3 @@
+#To run as scheduled task use the program powershell.exe and the argument -ExecutionPolicy Bypass "filepathtoscript"
 ﻿New-Item -Path C:\Users\schm2055\Desktop\Cleanup -ItemType Directory
 Get-ChildItem -Path C:\Users\schm2055\Desktop\* -Include *.txt, *.png, *.ppt*, *.jpeg, *.jpg, *.pdf, *.csv, *.xls*, *.pot*, *.xml*, *.doc*| Move-Item -Destination C:\Users\schm2055\Desktop\Cleanup
